61a36961a7b55c5121ea2db4fc555ffbcd32f8e9
liuzhen <redtomato158@gmail.com>
2019-09-12 13:30:58 +0000

    add color install script

--- install_basic_vimrc.sh
+++ install_basic_vimrc.sh
@@ -3,4 +3,6 @@ set -e
 
 cd ~/.vim_runtime
 cat ~/.vim_runtime/vimrcs/basic.vim > ~/.vimrc
+mkdir -p ~/.vim/colors
+cp ./colors/*.vim ~/.vim/colors/
 echo "Installed the Basic Vim configuration successfully! Enjoy :-)desert"
---
+++ install_color.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/sh
+set -e
+cd ~/.vim_runtime/
+mkdir -p ~/.vim/colors
+cp ./colors/*.vim ~/.vim/colors/
